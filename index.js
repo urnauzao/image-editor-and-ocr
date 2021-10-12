cbd960cedfac65bf8033fc18849f4b5aa41f553a
Urnauzão <julio.cesar.08.08.93@gmail.com>
2021-10-12 20:27:58 -0300

    download da imagem cortada

--- index.js
+++ index.js
@@ -22,38 +22,38 @@ app.use('/assets/webfonts', express.static(__dirname + '/src/webfonts'));
 
 // JS
 app.group("/assets/js", (router) => {
-  router.get('/jquery.js', function(req, res) {
-      res.sendFile(__dirname+'/node_modules/jquery/dist/jquery.js');
+  router.get('/jquery.js', function (req, res) {
+    res.sendFile(__dirname + '/node_modules/jquery/dist/jquery.js');
   });
-  router.get('/jquery.min.js', function(req, res) {
-      res.sendFile(__dirname+'/node_modules/jquery/dist/jquery.min.js');
+  router.get('/jquery.min.js', function (req, res) {
+    res.sendFile(__dirname + '/node_modules/jquery/dist/jquery.min.js');
   });
-  router.get('/cropper.js', function(req, res) {
-      res.sendFile(__dirname+'/node_modules/cropperjs/dist/cropper.js');
+  router.get('/cropper.js', function (req, res) {
+    res.sendFile(__dirname + '/node_modules/cropperjs/dist/cropper.js');
   });
-  router.get('/jquery-cropper.js', function(req, res) {
-      res.sendFile(__dirname+'/node_modules/jquery-cropper/dist/jquery-cropper.js');
+  router.get('/jquery-cropper.js', function (req, res) {
+    res.sendFile(__dirname + '/node_modules/jquery-cropper/dist/jquery-cropper.js');
   });
-  router.get('/bootstrap.min.js', function(req, res) {
-    res.sendFile(__dirname+'/node_modules/bootstrap/dist/js/bootstrap.min.js');
+  router.get('/bootstrap.min.js', function (req, res) {
+    res.sendFile(__dirname + '/node_modules/bootstrap/dist/js/bootstrap.min.js');
   });
-  router.get('/tesseract.min.js', function(req, res) {
+  router.get('/tesseract.min.js', function (req, res) {
     res.sendFile(__dirname + '/node_modules/tesseract.js/dist/tesseract.min.js');
   });
-  router.get('/sweetalert2.all.min.js', function(req, res) {
+  router.get('/sweetalert2.all.min.js', function (req, res) {
     res.sendFile(__dirname + '/node_modules/sweetalert2/dist/sweetalert2.all.min.js');
   });
 });
 
 // CSS
 app.group("/assets/css", (router) => {
-  router.get('/bootstrap.min.css', function(req, res) {
-    res.sendFile(__dirname+'/node_modules/bootstrap/dist/css/bootstrap.min.css');
-  });  
-  router.get('/cropper.css', function(req, res) {
-    res.sendFile(__dirname+'/node_modules/cropperjs/dist/cropper.css');
-  });  
-  router.get('/sweetalert2.css', function(req, res) {
-    res.sendFile(__dirname+'/node_modules/sweetalert2/dist/sweetalert2.min.css');
-  });  
+  router.get('/bootstrap.min.css', function (req, res) {
+    res.sendFile(__dirname + '/node_modules/bootstrap/dist/css/bootstrap.min.css');
+  });
+  router.get('/cropper.css', function (req, res) {
+    res.sendFile(__dirname + '/node_modules/cropperjs/dist/cropper.css');
+  });
+  router.get('/sweetalert2.min.css', function (req, res) {
+    res.sendFile(__dirname + '/node_modules/sweetalert2/dist/sweetalert2.min.css');
+  });
 });
